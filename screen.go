a82a72e8e431ae2ba4568b25340a9752bffc305b
masaki <ikeda.masaki@gmail.com>
2018-02-24 21:23:05 +0900

    scan from sidin

--- screen.go
+++ screen.go
@@ -1,11 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
 type screen struct {
 }
 
-func newScreen(i *input) *screen {
+func newScreen() *screen {
 
 	s := new(screen)
 
 	return s
+}
+
+func (s *screen) render(text string) {
+
+	if runtime.GOOS == "windows" {
+		cmd := exec.Command("cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	} else {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+
+	fmt.Print("> ")
 }
--- trans.go
+++ trans.go
@@ -1,7 +1,11 @@
 package main
 
+import (
+	"bufio"
+	"os"
+)
+
 type trans struct {
-	input  *input
 	screen *screen
 }
 
@@ -9,12 +13,46 @@ func newTrans() *trans {
 
 	t := new(trans)
 
-	t.input = newInput()
-	t.screen = newScreen(t.input)
+	t.screen = newScreen()
 
 	return t
 }
 
 func (t *trans) run() {
+
+	t.screen.render("")
+
+	doneCh := make(chan bool)
+	inputCh := make(chan string)
+	defer close(doneCh)
+	defer close(inputCh)
+
+	go func() {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+
+			if scanner.Text() == "quit" {
+				doneCh <- true
+				break
+
+			} else {
+
+				inputCh <- scanner.Text()
+			}
+		}
+	}()
+
+loop:
+	for {
+		select {
+
+		case <-doneCh:
+			break loop
+
+		case text := <-inputCh:
+			t.screen.render(text)
+
+		}
+	}
 
 }
